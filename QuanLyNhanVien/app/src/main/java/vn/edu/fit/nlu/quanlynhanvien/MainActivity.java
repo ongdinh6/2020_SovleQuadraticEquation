e88561e368719a578a9d941e36d479d5eb5892fa
ongdinh6 <ongdinh6@gmail.com>
2020-10-23 14:22:12 +0700

    sua loi array va fix cung huong doc

--- QuanLyNhanVien/app/src/main/java/vn/edu/fit/nlu/quanlynhanvien/MainActivity.java
+++ QuanLyNhanVien/app/src/main/java/vn/edu/fit/nlu/quanlynhanvien/MainActivity.java
@@ -29,46 +29,20 @@ public class MainActivity extends AppCompatActivity {
     Button btnAddEmployee;
     RadioGroup radioGroup;
     boolean isPartTime = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edtIDEmployee = findViewById(R.id.edt_IDEmployee);
-        edtNameEmployee = findViewById(R.id.edt_NameEmployee);
-        edtDayNum = findViewById(R.id.edt_DayNum);
-        //action for btnAdd
-        btnAddEmployee = findViewById(R.id.btn_AddEmployee);
-        btnAddEmployee.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                    Toast.makeText(getApplicationContext(), ""+edtIDEmployee.getText().toString(), Toast.LENGTH_SHORT).show();
-                if(edtIDEmployee.getText().toString() == null || edtNameEmployee.getText().toString() == null || edtDayNum.getText().toString() == null){
-                    Toast.makeText(getApplicationContext(), "Please input values!", Toast.LENGTH_SHORT).show();
-                }else {
-                    ArrayList<Employee>lst = getEmployee();
-                    employeeAdapter = new EmployeeAdapter(MainActivity.this, R.layout.activity_employee_layout, getEmployee());
-                    lvEmployee.setAdapter(employeeAdapter);
-                }
-            }
-        });
 
-    }
-
-    private ArrayList<Employee> getEmployee() {
-        lvEmployee = findViewById(R.id.listView_EmployeeInfor);
-        listEmployee = new ArrayList<>();
-
-        //getEmployeeInfor from action edit
         edtIDEmployee = findViewById(R.id.edt_IDEmployee);
         edtNameEmployee = findViewById(R.id.edt_NameEmployee);
         edtDayNum = findViewById(R.id.edt_DayNum);
 
         //event of radioButton
+        radioGroup = findViewById(R.id.rg_TypeOfEmployee);
         rdoFullTime = findViewById(R.id.rdo_FullTime);
         rdoPartTime = findViewById(R.id.rdo_PartTime);
 
-        radioGroup = findViewById(R.id.rg_TypeOfEmployee);
         radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -76,20 +50,43 @@ public class MainActivity extends AppCompatActivity {
                 int i = radioGroup.indexOfChild(v);
                 if (i == 0) {
                     isPartTime = false;
-                    Toast.makeText(getApplicationContext(), "you choose " + isPartTime, Toast.LENGTH_SHORT).show();
                 } else {
                     isPartTime = true;
-                    Toast.makeText(getApplicationContext(), "you choose " + isPartTime, Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        Employee e = new Employee();
-        e.setId(edtIDEmployee.getText().toString());
-        e.setName(edtNameEmployee.getText().toString());
-        e.setDayNum(Integer.parseInt(edtDayNum.getText().toString()));
-        e.setPartTime(isPartTime);
-        listEmployee.add(e);
 
-        return listEmployee;
+        //setListView
+        lvEmployee = findViewById(R.id.listView_EmployeeInfor);
+        //action for btnAdd
+        btnAddEmployee = findViewById(R.id.btn_AddEmployee);
+        listEmployee = new ArrayList<>();
+        btnAddEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtIDEmployee.getText().toString().equals("") || edtNameEmployee.getText().toString().equals("") || edtDayNum.getText().toString().equals("")) {
+                    String str ="";
+                    if(edtIDEmployee.getText().toString().equals("")){
+                        str = "id";
+                    }
+                    if(edtNameEmployee.getText().toString().equals("")){
+                        str = "name";
+                    }
+                    if(edtDayNum.getText().toString().equals("")){
+                        str = "day num";
+                    }
+                    Toast.makeText(getApplicationContext(), "Please input "+ str+ " values!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Employee e = new Employee();
+                    e.setId(edtIDEmployee.getText().toString());
+                    e.setName(edtNameEmployee.getText().toString());
+                    e.setDayNum(Integer.parseInt(edtDayNum.getText().toString()));
+                    e.setPartTime(isPartTime);
+                    listEmployee.add(e);
+                    employeeAdapter = new EmployeeAdapter(MainActivity.this, R.layout.activity_employee_layout, listEmployee);
+                    lvEmployee.setAdapter(employeeAdapter);
+                }
+            }
+        });
     }
 }
